b885ae3355d8cb2b10b9a9fc96aac5d44a2381bb
cameronmcnz <jurgapet@gmail.com>
2025-02-04 20:28:01 +0000

    sms

--- 2025-02-04 js 37/script.js
+++ 2025-02-04 js 37/script.js
@@ -1,5 +1,38 @@
-const mygtukas = document.querySelector("body");
+// Sukurkite formą, kurioje būtų vykdomas darbuotojų check-in atvykus į darbą
+// Forma turės laukelius:
+// Darbuotojo vardas - nuo 3 iki 50 simbolių, pirma raidė privalo būti didžioji, 
+// negali būti nei vieno skaičiaus, negali būti nei vieno tarpelio.
+// Darbuotojo pavardė - nuo 3 iki 50 simbolių, pirma raidė privalo būti didžioji, 
+// negali būti nei vieno skaičiaus.
+// Darbuotojo numeris - prasideda trimis didžiosiomis raidėmis, po trečiosios 
+// raidelės automatiškai padedamas "-" ir tuomet leistina įvestis - tik trys 
+// skaičiai.
 
-mygtukas.addEventListener("click", (event)=>{
-    console.log(event);
+// Po kiekvienu įvesties laukeliu - pagalbinis tekstas raudonomis arba žaliomis 
+// mažomis raidelėmis sufleruojantis, ar validacija sėkminga, ar kažko trūksta. 
+// Pakeitus įvesties laukelio reikšmę, pagalbinis tekstas turi atsinaujinti į kitą 
+// būseną, jei laukelyje įvesta reikšmė yra neteisinga, arba iš neteisingos 
+// pasikeitė į sėkmingą. Savo nuožiūra padarykite formą kiek įmanoma labiau 
+// patogesne naudotis. (tarkime apribokite leistinų įvesti raidžių kiekį iki 50)
+// Po forma esančio mygtuko pagalba užfiksuokite, kada įvyko check-in (datą ir laiką) 
+// bei išsaugokite objektą į bendrą sąrašą localStorage (tariamam tolimesniam 
+//     panaudojimui kituose programos vystymo etapuose)
+const nameElement = document.getElementById("name");
+const lastNameElement = document.getElementById("lastName");
+const idNumberElement = document.getElementById("idNumber");
+const buttonElement = document.getElementById("button");
+const dataElement = document.getElementById("data");
+
+const newGuess = document.createElement('p');
+addEventListener("change", (event) => {});
+
+onchange = (event) => {};
+
+function showTime(){
+    buttonElement.addEventListener("click", new Date);
+}
+console.log(showTime)
+
+document.querySelector("input").addEventListener("change", (event) => {
+	console.log("Įvesta įvesties laukelio reikšmė");
 });
--- 2025-02-04 js praktika-zaidimas/script.js
+++ 2025-02-04 js praktika-zaidimas/script.js
@@ -26,9 +26,140 @@
 // skaičiaus programa galvos skaičių (pvz., iki 10, iki 100 ar iki 
 // 1000)
 
-const startBtn = 
+const formBox = document.getElementById('formBox');
+const startBtn = document.getElementById('startBtn');
+const inputField = document.getElementById('number');
+const errorMsg = document.getElementById('errorMsg');
+const infoMsg = document.getElementById('infoMsg');
+const guessBtn = document.getElementById('submit');
+const historyList = document.getElementById('history-list');
+const winGif = document.getElementById('win');
 
+// kintamųjų inicijavimas
+let secretNumber = 0;
+let counter = 0;
 
-function rand(min, max){
-    return Math.floor(Math.random() * (max - min +1)) + min;
+// Stebėtojų prikabinimas prie mygtukų
+startBtn.addEventListener('click', newGame);
+guessBtn.addEventListener('click', makeGuess);
+
+// Generuojame random skaiciu nuo 1 iki 999
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Funkcija skirta pradėti žaidimą
+function newGame(){
+    //generuojam skaičių
+    secretNumber = rand(1, 999);
+    console.log(secretNumber);
+    // parodom įvedimo formą
+    formBox.style.opacity = '1';
+
+    // Išvalom klaidas
+    clearError();
+    // Išvalom ir gražinam stilių į pradinį įvedimo laukui
+    inputField.value = '';
+    inputField.style.borderColor = '';
+    //išvalom ir paslepiam info bloką
+    infoMsg.textContent ='';
+    infoMsg.style.opacity = '0';
+    
+    // Ištrinam ankstesnio žaidimo duomenys jei tokie buvo
+    historyList.innerHTML = '';
+    counter =0;
+    winGif.style.opacity = '0';
+
+    //suaktyvinam mygtuką
+    guessBtn.disabled = false;
+
+}
+
+// funkcija skirta patikrinti vartotojo įvedamą 
+function validateInput(){
+
+    //  tikrina įvedamą reikšmę ir neleidžia įvesti nieko kito tik skaičius
+    let sanitizedValue = inputField.value.replace(/[^0-9]/g, '');
+    inputField.value = sanitizedValue
+
+    // tikrina ar įvedama reikšmė ką nors turi. 
+    // Jei skaičius tai length bus didesnis, jei įvedinėjama raides ar kita tai bus 0
+    if(sanitizedValue.length === 0){
+        setError('Only numbers are required');
+        return null;
+    }
+     // Konvertuojama turima reikšmė į skaičių
+    const number = parseInt(sanitizedValue, 10);
+
+    // tikrinama ar vartotojas nepradeda nuo 0
+    if(number === 0){
+        setError('Number cannot start with 0');
+        return null;
+    }
+
+    // tikrinama ar skaičius nėra didesnis už 999
+    if(number > 999){
+        setError('The number cannot be greater than 999');
+        return null;
+    }
+
+    // isvalomas klaidu pranesimas
+    clearError();
+    return number;
+}
+
+// funkcija tikrinanti spejima
+function makeGuess(event){
+    // butina, kad siunciant forma nepersikrautu psl
+    event.preventDefault();
+    console.log('spejimas')
+    // dar karta patikrinamas ivesta reiksme
+    const number = validateInput();
+    if( number === null){
+        setError('Something is wrong');
+        return null;
+
+    } else if(number > secretNumber){
+        setHistory(number, 'the secret number is smaller');
+        return null;
+    }else if(number < secretNumber){
+        setHistory(number, 'the secret number is greater');
+        return null;
+    }
+
+    // atspetas skaicius, parodomas paveikslelis + blokuojamas mygtukas
+    setHistory(number, 'You guessed it, Yay !!!');
+    winGif.style.opacity ='1';
+    guessBtn.disabled = true;
+}
+
+// funkcija skirta spejimo informacijos isvedimui
+function setHistory(number, msg){
+    counter++;
+    const newGuess = document.createElement('p');
+    newGuess.textContent = `${counter}. ${number}: ${msg}`;
+    historyList.appendChild(newGuess);
+
+}
+
+// funkcija skirta parodyti klaidos pranesima
+function setError(message){
+    infoMsg.textContent = '';
+    infoMsg.style.opacity = '0';
+    errorMsg.textContent = message;
+    errorMsg.style.opacity = '1';
+    inputField.style.borderColor = 'red';
+    inputField.value = '';
+    guessBtn.disabled = true;
+
+}
+
+// funkcija skirta išvalyti klaidos pranesimus
+function clearError(){
+    errorMsg.textContent = '';
+    errorMsg.style.opacity = '0';
+    inputField.style.borderColor = 'green';
+    infoMsg.textContent = 'Enter a number from 1 to 999';
+    infoMsg.style.opacity = '1';
+    guessBtn.disabled = false;
 }
