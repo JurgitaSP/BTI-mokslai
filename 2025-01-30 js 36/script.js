c2365b3d4e9532dad865400d159d225f32542551
cameronmcnz <jurgapet@gmail.com>
2025-02-07 17:25:15 +0000

    sms

--- 2025-01-30 js 36/script.js
+++ 2025-01-30 js 36/script.js
@@ -34,42 +34,66 @@ const uzduotys = [{
 
 rodytiUzduotis();
 
-DOM.uzduotiesPridejimoMygtukas.addEventListener("click", ()=>{
-     const reiksme = DOM.uzduotiesPridejimoLaukelis.value;
-    if(reiksme.length >= 5){
-        uzduotys.push({title: reiksme, isFinished: false});
+DOM.uzduotiesPridejimoMygtukas.addEventListener("click", () => {
+    const reiksme = DOM.uzduotiesPridejimoLaukelis.value;
+    if (reiksme.length >= 5) {
+        uzduotys.push({ title: reiksme, isFinished: false });
         rodytiUzduotis();
     }
-       else alert("uzduoties pavadinimas turetu buti ne trumpesnis nei 5 simboliai");
-    
+    else alert("uzduoties pavadinimas turetu buti ne trumpesnis nei 5 simboliai");
+
 });
 
 //2. atvaizduoti aktyvias ir pabaigtas uzduotis
 //uzduotys paskirstomos pagal isFinished laukeli, jei false, prideti i aktyviu sarasa
 
-function rodytiUzduotis(){
-const nepabaigtosUzduotys = uzduotys.filter((uzduotis)=>!uzduotis.isFinished);//arba !uzduotis.isFinished - nebaigta uzduotis
-const pabaigtosUzduotys = uzduotys.filter((uzduotis)=>uzduotis.isFinished);
+function rodytiUzduotis() {
+    const nepabaigtosUzduotys = uzduotys.filter((uzduotis) => !uzduotis.isFinished);//arba !uzduotis.isFinished - nebaigta uzduotis
+    const pabaigtosUzduotys = uzduotys.filter((uzduotis) => uzduotis.isFinished);
 
-let nepabaigtuUzduociuHtml = "";
-let pabaigtuUzduociuHtml = "";
+    let nepabaigtuUzduociuHtml = "";
+    let pabaigtuUzduociuHtml = "";
 
-nepabaigtosUzduotys.forEach((nepabaigtaUzduotis)=>{
-    nepabaigtuUzduociuHtml += `<li><span>${nepabaigtaUzduotis.title}</span>
-    <button>Istrinti</button>
-    <button>Uzduotis atlikta</button></li>`;
+    nepabaigtosUzduotys.forEach((nepabaigtaUzduotis) => {
+        nepabaigtuUzduociuHtml += `<li>
+        <span>${nepabaigtaUzduotis.title}</span>
+    <button onclick ="istrintiUzduoti('${nepabaigtaUzduotis.title}')">Istrinti</button>
+    <button onclick ="pabaigtiUzduoti('${nepabaigtaUzduotis.title}')">Uzduotis atlikta</button>
+    </li>`;
 
-});
-pabaigtosUzduotys.forEach((pabaigtaUzduotis)=>{
-    pabaigtuUzduociuHtml += `<li><span>${pabaigtaUzduotis.title}</span>
-    <button>Istrinti</button>`;
+    });
 
-});
+    pabaigtosUzduotys.forEach((pabaigtaUzduotis) => {
+        pabaigtuUzduociuHtml += `<li>
+        <span>${pabaigtaUzduotis.title}</span>
+    <button onclick ="istrintiUzduoti('${pabaigtaUzduotis.title}')">Istrinti</button>
+    </li>`;
+    });
 
- DOM.nepabaigtuUzduociuSarasas.innerHTML = nepabaigtuUzduociuHtml;
- DOM.pabaigtuUzduociuSarasas.innerHTML = pabaigtuUzduociuHtml;
+    DOM.nepabaigtuUzduociuSarasas.innerHTML = nepabaigtuUzduociuHtml;
+    DOM.pabaigtuUzduociuSarasas.innerHTML = pabaigtuUzduociuHtml;
 }
 
-function istrintiUzduoti(title){
+function istrintiUzduoti(title) {
+    const uzduotiesIndeksas = uzduotys.findIndex(
+        (uzduotis) => title.toLowerCase() == uzduotis.title.toLowerCase());//grazina skaiciu - indeksa
+    if (uzduotiesIndeksas !== -1) { //jei i splicenurodysime -1, gausime klaida,be to gali istrinti
+        //paskuti elementa
+        uzduotys.splice(uzduotiesIndeksas, 1);
+        rodytiUzduotis();
+    }
+    else console.error("Trinamos uzduoties pavadinimas uzduociu masyve buvo nerastas");
+}
+//uzduotis is nepabaigtu uzduociu persikeltu i pabaigtas isfinished false i true
+//paspaudus mygtuka "uzduotis atlikta", ji persikelia i uzbaigtu uzduociu sarasa
+//atrandame pacia uzduoti, pakeciame false i true ir pridedam prie  masyvo
 
+// 4. atnaujinti uzduoties statusa
+function pabaigtiUzduoti(title){
+    const uzduotis = uzduotys.find(
+        (uzduotis) => title.toLowerCase() == uzduotis.title.toLowerCase())&&
+        uzduotis.isFinished === false;
+
+uzduotis.isFinished = true;
+rodytiUzduotis();
 }
