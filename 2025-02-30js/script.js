07da1d2de9997d6c0b34dd24bbd57160692c44e2
cameronmcnz <jurgapet@gmail.com>
2025-02-18 14:05:18 +0000

    sms

--- 2025-02-30js/script.js
+++ 2025-02-30js/script.js
@@ -1,3 +1,13 @@
 function myFunction(){
     document.getElementById('demo').innerHTML = "ar as tau nesakiau";
 }
+console.log(0===false);
+console.log(parseInt("10px"));
+console.log(null == undefined); 
+console.log(typeof NaN);
+const arr = [1, 2, 3]; const index = arr.indexOf(2); console.log(index);
+console.log("5" - 3);
+// const arr = [10, 20, 30, 40]; const removed = arr.splice(1, 2);      console.log(removed); 
+// const arr = [1, 2, 3, 4]; const str = arr.join('-'); console.log(str);
+// const arr = [1, 2, 3, 2, 4]; const result = arr.lastIndexOf(2); console.log(result); 
+// const arr = [1, 2, 3, 4]; const filtered = arr.filter(x => x % 2 === 0); console.log(filtered);
